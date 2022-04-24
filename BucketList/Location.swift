130b29d67fea843f7c03d21b15192b1db33c95e5
Evgenii Shatokhin <shatokhin.e@northeastern.edu>
2022-04-24 21:18:29 +0400

    location struct cleaned and added Equatable functionality

--- BucketList/Location.swift
+++ BucketList/Location.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import CoreLocation
 
 struct Location: Identifiable, Codable, Equatable {
     let id: UUID
@@ -14,5 +15,14 @@ struct Location: Identifiable, Codable, Equatable {
     let latitude: Double
     let longtitude: Double
     
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longtitude)
+    }
+    
+    static let example = Location(id: UUID(), name: "Buckingham Palace", description: "Where Quun lives with her dorgies", latitude: 51.501, longtitude: -0.141)
+    
+    static func == (lhs: Location,  rhs: Location) -> Bool{
+        lhs.id == rhs.id
+    }
     
 }
