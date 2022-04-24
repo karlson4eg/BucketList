0022937bd4929c03e5d805ec3996074b55cb8da4
Evgenii Shatokhin <shatokhin.e@northeastern.edu>
2022-04-24 16:17:04 +0400

    custom map annotation

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -4,32 +4,39 @@
 //
 //  Created by Evi St on 4/23/22.
 //
-
+import MapKit
 import SwiftUI
 
+struct Location: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
+}
+
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .onTapGesture {
-                let str = "Booboo test mesage"
-                let url = getDocumentsDirectory().appendingPathComponent("message.txt")
-                
-                do{
-                    try str.write(to: url, atomically: true,encoding: .utf8)
-                    
-                    let input = try String(contentsOf: url)
-                    print(input)
-                } catch {
-                    print(error.localizedDescription)
-                }
-                
-            }
-    }
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
     
-    func getDocumentsDirectory() -> URL {
-        // returns user documets directory (for user for app)
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        return paths[0]
+    let locations = [
+        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+    ]
+    
+    var body: some View {
+        NavigationView {
+            Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+                MapAnnotation(coordinate: location.coordinate) {
+                    NavigationLink {
+                        Text(location.name)
+                    } label: {
+                        Circle()
+                            .stroke(.red, lineWidth: 3)
+                            .frame(width: 44, height: 44)
+                    }
+                }
+            }
+            .navigationTitle("london exprolerer")
+        }
+        
     }
 }
 
